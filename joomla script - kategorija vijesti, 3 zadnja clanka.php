5940a3c2a22d010d7266e2a78b84dca42c6a8d4b
Darijan <dvertovs@foi.hr>
2016-02-14 11:41:00 +0100

    vijesti skripta, descending by time created

---
+++ joomla script - kategorija vijesti, 3 zadnja clanka.php
@@ -0,0 +1,32 @@
+<?php
+//dohvati Joomla database objekt
+$db = JFactory::getDbo();
+
+//dohvati novi Query (upit) objekt iz db (prethodno dohvacenog) objekta
+$query = $db->getQuery(true);
+
+
+$query->select(array('id','title','introtext'));
+/*
+*
+* TO DO: promjeniti kategorija id 
+*
+*/
+$catId = 10;
+$query->from($db->quoteName('#__content'));
+//$query->where($db->quoteName('published') . ' = 1'); //opcionalno: ako zelimo provjeriti jeli kategorija 'published'
+$query->where($db->quoteName('catid') . ' = ' . $catId);
+$query->order($db->quoteName('created') . ' DESC');
+$query->setLimit('3');
+
+//postavljanje definiranog upita u db objekt, zapravo: izvrsavanje upita
+$db->setQuery($query);
+//dohvacanje rezultat
+$results = $db->loadObjectList();
+foreach($results as $key=>$val)
+{
+	echo "<h1>".$val->title."</h1>";
+	echo $val->introtext;
+}
+
+?>
