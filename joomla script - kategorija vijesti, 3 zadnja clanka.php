8d39d764b2d72c2f66e532c1d1b7a1d386022ab2
Darijan <dvertovs@foi.hr>
2016-02-19 15:46:02 +0100

    get menu id

--- joomla script - kategorija vijesti, 3 zadnja clanka.php
+++ joomla script - kategorija vijesti, 3 zadnja clanka.php
@@ -1,18 +1,21 @@
 <?php
+
+
+$link = "index.php?option=com_content&view=category&layout=blog&id=86";
+$poljeLinkova = explode("=", $link);
+echo end($poljeLinkova);
+
+
+
 //dohvati Joomla database objekt
 $db = JFactory::getDbo();
 
-//dohvati novi Query (upit) objekt iz db (prethodno dohvacenog) objekta
-$query = $db->getQuery(true);
-
-
-
 /*
 *
-* TO DO: promjeniti kategorija id 
+* TO DO: promjeniti menu id 
 *
 */
-$catId = 10;
+$menuId = 1;
 /*
 *
 * TO DO: set offset
@@ -26,29 +29,43 @@ $offset = 0;
 */
 $limit = 3;
 
-$catQuery = $db->getQuery(true);
-$catQuery->select('alias');
-$catQuery->from($db->quoteName('#__categories'));
-$catQuery->where($db->quoteName('id') . ' ='.$catId);
-$db->setQuery($catQuery);
-$catAlias = $db->loadResult();
+//dohvati novi Query (upit) objekt iz db (prethodno dohvacenog) objekta
+$menuQuery = $db->getQuery(true);
+$menuQuery->select('link,alias');
+$menuQuery->from($db->quoteName('#__menu'));
+$menuQuery->where($db->quoteName('id') . ' ='.$menuId);
+//postavljanje definiranog upita u db objekt, zapravo: izvrsavanje upita
+$db->setQuery($menuQuery);
+//dohvacanje rezultat
+$menuResult = $db->loadObjectList()[0];
+$menuAlias = $menuResult->alias;
 
-$query->select('id,alias,title,introtext');
-$query->from($db->quoteName('#__content'));
-$query->where($db->quoteName('state') . ' = 1');
-$query->where($db->quoteName('catid') . ' = ' . $catId);
-$query->order($db->quoteName('created') . ' DESC');
+$linkArray = explode("=", $menuResult->link);
+$catId = end($poljeLinkova);
+
+$query = $db->getQuery(true);
+
+$query->select(array('content.id', 'content.hits', 'content.alias AS content_alias', 'content.title AS content_title', 'content.introtext', 'content.created', 'users.username', 'categ.title AS categ_title'));
+$query->from($db->quoteName('#__content', 'content'));
+$query->join('INNER', $db->quoteName('#__users', 'users') . ' ON (' . $db->quoteName('content.created_by') . ' = ' . $db->quoteName('users.id') . ')');
+$query->join('LEFT', $db->quoteName('#__categories', 'categ') . ' ON (' . $db->quoteName('categ.id') . ' = ' . $db->quoteName('content.catid') . ')');
+$query->where($db->quoteName('content.state') . ' = 1');
+$query->where($db->quoteName('content.catid') . ' = ' . $catId);
+$query->order($db->quoteName('content.created') . ' DESC');
 $query->setLimit($limit,$offset);
 
-//postavljanje definiranog upita u db objekt, zapravo: izvrsavanje upita
 $db->setQuery($query);
-//dohvacanje rezultat
 $results = $db->loadObjectList();
+
 foreach($results as $key=>$val)
 {
 	$id = $val->id;
-	$alias = $val->alias;
-	$articleTitle = $val->title;
+	$alias = $val->content_alias;
+	$articleTitle = $val->content_title;
+	$categTitle = $val->categ_title;
+	$articleHits = $val->hits;
+	$username = $val->username;
+	$created = $val->created;
 
 	$doc = new DOMDocument();
 
@@ -63,8 +80,11 @@ foreach($results as $key=>$val)
 	{
 		echo "<img src='".htmlentities($object->getNamedItem('src')->nodeValue)."' alt='".$articleTitle."' title='".$articleTitle."'>";
 	}
-
-	echo "<a href='http://gradimozadar.hr/".$catAlias."/".$id."-".$alias."'><h1>".$articleTitle."</h1></a>";
+	echo "<div>".$categTitle."</div>";
+	echo "<div>".$username."</div>";
+	echo "<div>".$articleHits."</div>";
+	echo "<div>".$created."</div>";
+	echo "<a href='http://gradimozadar.hr/".$menuAlias."/".$id."-".$alias."'><h1>".$articleTitle."</h1></a>";
 
 }
 
