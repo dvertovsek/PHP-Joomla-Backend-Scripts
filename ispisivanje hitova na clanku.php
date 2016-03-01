c50b7b1f5963032ac71a26bc2e1f22bba096a272
Darijan <dvertovs@foi.hr>
2016-03-01 14:33:37 +0100

    hit count

---
+++ ispisivanje hitova na clanku.php
@@ -0,0 +1,24 @@
+<?php
+
+	$url = htmlspecialchars($_SERVER['REQUEST_URI']);
+
+	$arr = explode('/',$url);
+	$articleId = explode('-',$arr[count($arr)-1])[0];
+
+	echo $articleId;
+
+	if(is_numeric($articleId))
+	{
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select(array('hits'));
+		$query->from($db->quoteName('#__content'));
+		$query->where($db->quoteName('id') . ' = '.(int)$articleId);
+
+		$db->setQuery($query);
+		$hitCount = $db->loadResult();
+
+		echo "<div>".$hitCount."</div>"
+	}
+?>
